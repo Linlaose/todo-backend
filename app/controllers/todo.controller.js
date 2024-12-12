1ee6ced885b2f566f3ee2084c58c38cd9a71b987
Ryan <aa0900144903@gmail.com>
2024-12-12 22:00:41 +0800

    feat(todos): add handling logic for query conditions 🆕
    在 `app/controllers/todo.controller.js` 中，更新 `getAll` 函數以處理查詢參數。

--- app/controllers/todo.controller.js
+++ app/controllers/todo.controller.js
@@ -3,12 +3,18 @@ const Todos = db.todos;
 
 exports.getAll = async (req, res) => {
   try {
-    const order_by = req?.query.order_by;
+    const { order_by, ...query } = req.query;
+    const where = Object.entries(query).reduce((acc, [key, value]) => {
+      if (value === "true") return { ...acc, [key]: true };
+      if (value === "false") return { ...acc, [key]: false };
+      return { ...acc, [key]: value };
+    }, {});
     if (!order_by)
       return res.status(400).json({ message: "order_by is required" });
     const [column, direction] = order_by.trim().split(" ");
     const todos = await Todos.findAll({
       order: [[column, direction.toUpperCase()]],
+      where,
     });
     res
       .status(200)
